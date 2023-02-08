143b7cb83a31ba57bbc92b294d3d8e57ed36f61e
Egor Koksharov <yegor.koksharov@gmail.com>
2023-02-08 22:41:44 +0300

    styled button

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,10 +1,15 @@
 import { FC } from "react";
+import { Button } from "../ui/button";
 import { TextBlock } from "../ui/typography";
 
 export const MainPage: FC = () => {
+  const onClick = () => console.log("click event!");
   return (
-    <TextBlock renderTag="h1" size="huge" color="danger">
-      this is text block
-    </TextBlock>
+    <Button primary onClick={onClick}>
+      hello
+      <TextBlock renderTag="h1" size="huge" color="danger">
+        this is text block
+      </TextBlock>
+    </Button>
   );
 };
--- src/ui/box.tsx
+++ src/ui/box.tsx
@@ -13,26 +13,49 @@ export interface BoxProps {
   pr?: number;
 }
 
-export const getBoxStyles = css<BoxProps>((props) => {
-  const { defaultSpacing } = props.theme.layout;
+export const calcSpacing = (
+  defaultSpacing: number | string,
+  multiplier: number
+) => {
   const digitsRegexp = /^[\d.]+/;
   const unitsRegexp = /\D*$/;
   const digits = parseFloat(
     (digitsRegexp.exec(`${defaultSpacing}`) || ["0"])[0] || "0"
   );
   const units = (unitsRegexp.exec(`${defaultSpacing}`) || [""])[0] || "px";
+  return `${digits * multiplier}${units}`;
+};
+
+export const getBoxStyles = css<BoxProps>((props) => {
+  const { defaultSpacing } = props.theme.layout;
 
   return {
-    ...(props.m ? { margin: `${props.m * digits}${units}` } : {}),
-    ...(props.mb ? { marginBottom: `${props.mb * digits}${units}` } : {}),
-    ...(props.mt ? { marginTop: `${props.mt * digits}${units}` } : {}),
-    ...(props.ml ? { marginLeft: `${props.ml * digits}${units}` } : {}),
-    ...(props.mr ? { marginRight: `${props.mr * digits}${units}` } : {}),
-    ...(props.p ? { padding: `${props.p * digits}${units}` } : {}),
-    ...(props.pb ? { paddingBottom: `${props.pb * digits}${units}` } : {}),
-    ...(props.pt ? { paddingTop: `${props.pt * digits}${units}` } : {}),
-    ...(props.pl ? { paddingLeft: `${props.pl * digits}${units}` } : {}),
-    ...(props.pr ? { paddingRight: `${props.pr * digits}${units}` } : {}),
+    ...(props.m ? { margin: calcSpacing(defaultSpacing, props.m) } : {}),
+    ...(props.mb
+      ? { marginBottom: calcSpacing(defaultSpacing, props.mb) }
+      : {}),
+    ...(props.mt ? { marginTop: calcSpacing(defaultSpacing, props.mt) } : {}),
+    ...(props.ml ? { marginLeft: calcSpacing(defaultSpacing, props.ml) } : {}),
+    ...(props.mr ? { marginRight: calcSpacing(defaultSpacing, props.mr) } : {}),
+    ...(props.p ? { padding: calcSpacing(defaultSpacing, props.p) } : {}),
+    ...(props.pb
+      ? { paddingBottom: calcSpacing(defaultSpacing, props.pb) }
+      : {}),
+    ...(props.pt ? { paddingTop: calcSpacing(defaultSpacing, props.pt) } : {}),
+    ...(props.pl ? { paddingLeft: calcSpacing(defaultSpacing, props.pl) } : {}),
+    ...(props.pr
+      ? { paddingRight: calcSpacing(defaultSpacing, props.pr) }
+      : {}),
+    // ...(props.m ? { margin: `${props.m * digits}${units}` } : {}),
+    // ...(props.mb ? { marginBottom: `${props.mb * digits}${units}` } : {}),
+    // ...(props.mt ? { marginTop: `${props.mt * digits}${units}` } : {}),
+    // ...(props.ml ? { marginLeft: `${props.ml * digits}${units}` } : {}),
+    // ...(props.mr ? { marginRight: `${props.mr * digits}${units}` } : {}),
+    // ...(props.p ? { padding: `${props.p * digits}${units}` } : {}),
+    // ...(props.pb ? { paddingBottom: `${props.pb * digits}${units}` } : {}),
+    // ...(props.pt ? { paddingTop: `${props.pt * digits}${units}` } : {}),
+    // ...(props.pl ? { paddingLeft: `${props.pl * digits}${units}` } : {}),
+    // ...(props.pr ? { paddingRight: `${props.pr * digits}${units}` } : {}),
   };
 });
 
